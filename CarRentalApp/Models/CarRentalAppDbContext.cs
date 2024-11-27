4d535949554d4b291de03cfe625431e23a33f81f
Jana Saa <jana.abusaa@gmail.com>
2024-11-27 02:55:48 +0200

    Add database context

---
+++ CarRentalApp/Models/CarRentalAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalApp.Models
+{
+    public class CarRentalAppDbContext : IdentityDbContext
+    {
+        public CarRentalAppDbContext(DbContextOptions<CarRentalAppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- CarRentalApp/Program.cs
+++ CarRentalApp/Program.cs
@@ -1,6 +1,14 @@
+using CarRentalApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CarRentalAppDbContext>(options =>
+    {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultDB"));
+    });
 
 var app = builder.Build();
 
